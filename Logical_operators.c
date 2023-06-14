e931c855be467061a363c531ebab1c8f72790a42
Samiksha Kuchewar <134524328+Samikshhha@users.noreply.github.com>
2023-06-14 14:18:30 +0530

    Create Logical_operators.c

---
+++ Logical_operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int age;
+    
+    printf ("enter your age\n");
+    scanf("%d", &age);
+    
+    if (age <= 70 && age>=18)
+    {
+        printf("yoe are above 18 and below 70, you can derive\n");
+    }
+    else
+    {
+        printf("you can drive\n");
+    }
+    return 0;
+}
