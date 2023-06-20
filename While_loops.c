ffa159bc764997a0760e9d419bd6043e5508a74a
Samiksha Kuchewar <134524328+Samikshhha@users.noreply.github.com>
2023-06-20 18:29:59 +0530

    Create While_loops.c

---
+++ While_loops.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("hello ");
+    printf("world\n");
+    int a = 1;
+    printf("%d\n", a);
+    
+    a++;
+    printf("%d\n", a);
+    
+    a++;
+    printf("%d\n", a);
+    
+    a++;
+    printf("%d\n", a);
+    
+    a++;
+    printf("%d\n", a);
+    
+    
+    return 0;
+    
+}
