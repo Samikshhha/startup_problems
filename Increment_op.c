a386c4e3f3c36d29d9afd5a8653a214648073adb
Samiksha Kuchewar <134524328+Samikshhha@users.noreply.github.com>
2023-06-21 18:03:02 +0530

    Create Increment_op.c

---
+++ Increment_op.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int i = 5;
+    printf("the value after i++ is %d\n", ++i);
+    
+    printf("the value of i is %d\n", i);
+    
+    printf("the value of i is %d\n", i);
+    
+    return 0;
+
+}
