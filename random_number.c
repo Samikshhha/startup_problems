bc6a3a5ad18eb2f120f285d5cf8b2b054f5be15c
Samiksha Kuchewar <134524328+Samikshhha@users.noreply.github.com>
2023-06-22 16:51:23 +0530

    Create random_number.c

---
+++ random_number.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+    int number;
+    srand(time(0));
+    
+    number = rand()%100 + 1;
+   
+    printf("the number is %d", number);
+    
+    return 0;
+    
+
+}
+    
+    
