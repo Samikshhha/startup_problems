0a797d84b151d466e2b0339fbec58d1b12c9e458
Samiksha Kuchewar <134524328+Samikshhha@users.noreply.github.com>
2023-09-20 19:13:36 +0530

    Create Nested if-else.c

---
+++ Nested if-else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter three value for a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b);
+{
+    if(a>c)
+{
+    printf("a is grater");
+}
+else
+{
+    printf("c is greater");
+}
+}
+{
+    printf("b is greater");
+}
+    return 0;
+}
